067d963a1fba005ec57dd9cdcb479f9bc5fe8340
d9magai <d9magai@gmail.com>
2025-06-01 23:05:58 +0900

    bookrepository

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -5,18 +5,26 @@ use std::sync::Arc;
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check_repository: Arc<dyn HealthCheckRepository>,
+    book_repository: Arc<dyn kernel::repository::book::BookRepository>,
 }
 
 impl AppRegistry {
     pub fn new(pool: ConnectionPool) -> Self {
         let health_check_repository: Arc<HealthCheckRepositoryImpl> =
             Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
+        let book_repository: Arc<dyn kernel::repository::book::BookRepository> =
+            Arc::new(adapter::repository::book::BookRepositoryImpl::new(pool));
         Self {
             health_check_repository,
+            book_repository,
         }
     }
 
     pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
         self.health_check_repository.clone()
+    }
+
+    pub fn book_repository(&self) -> Arc<dyn kernel::repository::book::BookRepository> {
+        self.book_repository.clone()
     }
 }
