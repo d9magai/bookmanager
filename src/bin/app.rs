505622c2de4f39740c072e1b8a685e144554e0a4
d9magai <d9magai@gmail.com>
2025-06-08 16:23:12 +0900

    error

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -6,6 +6,7 @@ use registry::AppRegistry;
 use shared::env::{Environment, which};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tracing::Level;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -34,15 +35,35 @@ fn init_logger() -> Result<()> {
     Ok(())
 }
 
+use anyhow::Context;
+use tower_http::LatencyUnit;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+
 async fn bootstrap() -> Result<()> {
     let pool = connect_database_with().await;
     let registry = AppRegistry::new(pool);
     let app = Router::new()
         .merge(build_health_check_routes())
         .merge(build_book_routers())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Micros),
+                ),
+        )
         .with_state(registry);
+
     let addr = SocketAddr::new(std::net::Ipv4Addr::LOCALHOST.into(), 3000);
     let listener = TcpListener::bind(addr).await?;
     println!("Listengin on {}", addr);
-    axum::serve(listener, app).await.map_err(Error::from)
+    axum::serve(listener, app)
+        .await
+        .context("Unexpected error happend in server")
+        .inspect_err(|e| {
+            tracing::error!(error.cause_chain = ?e, error.message = %e, "Unexpected error in server");
+        })
 }
