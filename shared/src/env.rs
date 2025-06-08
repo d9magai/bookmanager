571ca801e4a07901d06c08fc28ba9f8fe5febd02
d9magai <d9magai@gmail.com>
2025-06-08 10:31:14 +0900

    env

---
+++ shared/src/env.rs
@@ -0,0 +1,22 @@
+#[derive(Default, EnumString)]
+#[strum(serialize_all = "lowercase")]
+pub enum Environment {
+    Development,
+    Production,
+}
+
+pub fn which() -> Environment {
+    #[cfg(debug_assertions)]
+    let default_env = Environment::Development;
+    #[cfg(not(debug_assertions))]
+    let default_env = Environment::Production;
+
+    match std::env::var("ENV") {
+        Ok(env) => match env.as_str() {
+            "development" => Environment::Development,
+            "production" => Environment::Production,
+            _ => default_env, // Use default_env if not recognized
+        },
+        Err(_) => default_env, // Use default_env if ENV variable is not set
+    }
+}
